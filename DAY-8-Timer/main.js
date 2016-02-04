a42bd4319c1ca80cea96f7d61f212d6b52c9c6ba
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-04 15:54:38 +0800

    Almost done. To-do: Animation

--- DAY-8-Timer/main.js
+++ DAY-8-Timer/main.js
@@ -2,33 +2,90 @@ var Time = React.createClass({
 	timeFormat: function(t) {
 		return t < 10 ? "0"+t : t;
 	},
-	getNowTime: function() {
-		var now = new Date();
-		var min = this.timeFormat( now.getMinutes() );
-		var sec = this.timeFormat( now.getSeconds() );
+	render: function() {
+		return (
+			<div className="now">
+				{this.timeFormat(this.props.min)}  :  {this.timeFormat(this.props.sec)}
+			</div>
+		);
+	}
+});
 
-		return [min, sec];
-	},
+var countTimer;
+var options = [];
+for( var i = 0; i < 60; i++ ) {
+	options.push( <option value={i}>{i}</option> );
+}
+
+var Minutes = React.createClass({
+	render: function() {
+		return (
+			<select id="min" className="form-control" onChange={this.props.onChange}>
+				{options}
+			</select>
+		);
+	}
+});
+
+var Seconds = React.createClass({
+	render: function() {
+		return (
+			<select id="sec" className="form-control" onChange={this.props.onChange}>
+				{options}
+			</select>
+		);
+	}
+});
+
+var Card = React.createClass({
+	render: function() {
+		return (
+			<div className="card">
+				<div className="title">SET TIMER</div>
+				<h5>MINUTES:</h5>
+				<Minutes onChange={this.props.onMinutesChange} />
+				<h5>SECONDS:</h5>
+				<Seconds onChange={this.props.onSecondsChange} />
+				<div className="btn" onClick={this.props.timerStart}>GO</div>
+			</div>
+		);
+	}
+});
+
+var Timer = React.createClass({
 	getInitialState: function() {
-		var timeArray = this.getNowTime();
-		document.body.style.backgroundColor = '#'+timeArray.join('');
-		return { time: timeArray.join('  :  ') }
+		return { minutes: 0, seconds: 0, total: 0 };
 	},
-	componentDidMount: function() {
-		setInterval( function() {
-			var timeArray = this.getNowTime();
-			this.setState({ time: timeArray.join('  :  ') });
+	handleMinutesChange: function(e) {
+		this.setState({ minutes: e.target.value });
+	},
+	handleSecondsChange: function(e) {
+		this.setState({ seconds: e.target.value });
+	},
+	startTimer: function() {
+		this.setState({ total: parseInt(this.state.minutes*60)+parseInt(this.state.seconds) });
+		countTimer = setInterval( function(){
+			var newTotal = this.state.total-1
+
+			this.setState({
+				minutes: Math.floor(newTotal/60),
+				seconds: newTotal%60,
+				total: this.state.total-1
+			});
+
+			if( newTotal == 0 ) { clearInterval( countTimer ); }
 		}.bind(this), 1000);
 	},
 	render: function() {
 		return (
-			<div className="now">
-				{this.state.time}
+			<div>
+				<Time min={this.state.minutes} sec={this.state.seconds} />
+				<Card onMinutesChange={this.handleMinutesChange} onSecondsChange={this.handleSecondsChange} timerStart={this.startTimer} />
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<Time />, document.getElementById('container')
+	<Timer />, document.getElementById('container')
 );
