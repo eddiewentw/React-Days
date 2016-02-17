b122d1da9905f2eb9af16b641bbb27ed49139770
EddieWen-Taiwan <m20021991y@gmail.com>
2016-02-17 10:47:46 +0800

    Use gulp to parse ES2015

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var babel = require('gulp-babel');
 var replaceHTML = require('gulp-html-replace');
 var rename = require('gulp-rename');
 
-var targetDAY = 'DAY-9-ColorBoxes/';
+var targetDAY = 'DAY-10-Scroll/';
 
 gulp.task( 'default', ['babel', 'html'] );
 
