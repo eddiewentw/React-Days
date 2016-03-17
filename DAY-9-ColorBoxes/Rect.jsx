9305a771994653974f6638da87f2ca522e83c8aa
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-17 22:29:10 +0800

    Split .jsx file

---
+++ DAY-9-ColorBoxes/Rect.jsx
@@ -0,0 +1,27 @@
+export class Rect extends React.Component {
+
+	constructor() {
+		super();
+
+		this._mouseEnter = this._mouseEnter.bind(this); // cool
+		this.state = {
+			rr: 0, gg: 0, aa: 0
+		};
+	}
+
+	_randomColor() {
+		return parseInt(Math.random()*255);
+	}
+
+	_mouseEnter() {
+		this.setState({
+			rr: this._randomColor(), gg: this._randomColor(), aa: this._randomColor()
+		});
+	}
+
+	render() {
+		let rectBackgroundStyle = { backgroundColor: `rgb( ${this.state.rr}, ${this.state.gg}, ${this.state.aa} )` };
+		return <div className='rect' onMouseEnter={this._mouseEnter} onTouchEnd={this._mouseEnter} style={rectBackgroundStyle}></div>;
+	}
+
+}
